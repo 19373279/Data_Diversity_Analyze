8aad65d9a3d694cf291eaa78ffd84736c87a13d0
zhoulanzhi <819026430@qq.com>
2024-09-06 23:21:40 +0800

    2024_9_6

---
+++ src/evaluation_methods/image_difference.py
@@ -0,0 +1,13 @@
+class NumberOfPictures:
+    @staticmethod
+    def get_numbers(self, dataset):
+        result = 100
+        if dataset is None:
+            result = 0
+        return result
+
+
+class OnePicture:
+    @staticmethod
+    def get_realism(self):
+        return -1
